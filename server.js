19470a36b79f7f0138d709912bf7dacb8e0dc854
King <gxnews8@gmail.com>
2017-04-27 19:37:03 -0700

    Adding passport

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var passport     = require('passport');
+var session      = require('express-session');
 var path = require('path');
 var port = 8000;
+
+// PASSPORT
+require('./server/config/passport.js')(passport);
 
 app.use(express.static(path.join(__dirname,'/client')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(bodyParser.json());
+app.use(session({ secret: 'Joblog secret', name: 'Joblog' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(port, function(){
     console.log(`JOBLOG - listening to ${port}`)
