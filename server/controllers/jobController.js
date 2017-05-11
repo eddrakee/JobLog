bba60757e04cc6f53be048a2a50a7d13319ee78c
susmithathota <sridhardutta@Sridhars-MacBook-Air.local>
2017-05-11 13:30:53 -0700

    modified add Job using self and added allJobs functionality

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -11,9 +11,20 @@ module.exports = (function(){
           console.log(err);
         }
         else{
+          console.log('added new job'+ req.body);
           res.json(data);
         }
       })
+    },
+    allJobs: function(req,res){
+        Job.find({},function(err,data){
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.json(data);
+            }
+        })
     }
   }
 })();
