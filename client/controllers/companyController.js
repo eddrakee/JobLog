9a50cd42825e017fef6012d4500ffab887a81da4
AvsCode <avsean@gmail.com>
2017-05-11 13:04:28 -0700

    adding some company controller functionality

--- client/controllers/companyController.js
+++ client/controllers/companyController.js
@@ -3,11 +3,12 @@ angular.module("app").controller("companyController", companyController);
 function companyController($location, companyFactory){
   var self = this;
 
-  self.companyName;
-  self.companyAdd;
-  self.companyPhone;
+  // self.companyName;
+  // self.companyAdd;
+  // self.companyPhone;
 
-  self.submit = () => {
+  self.addCompany = () => {
+    console.log("CALLING ADD COMPANY");
     companyFactory.submit(self.companyName, self.companyAdd, self.companyPhone, function(returnedData){
       console.log(returnedData);
     });
